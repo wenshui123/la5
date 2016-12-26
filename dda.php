4dfd14ebcf6bc626541ba336c1a4109c7439f700
zws <zws>
2016-12-26 16:40:49 +0800

    初始化

---
+++ dda.php
@@ -0,0 +1,249 @@
+Array
+(
+[app_status] => debug
+[app_file_case] => 1
+[app_autoload_path] =>
+[app_tags_on] => 1
+[app_sub_domain_deploy] =>
+[app_sub_domain_rules] => Array
+(
+)
+
+[app_sub_domain_deny] => Array
+(
+)
+
+[app_group_list] =>
+[app_group_mode] => 0
+[app_group_path] => Modules
+[action_suffix] =>
+[cookie_expire] => 43200
+[cookie_domain] => touch.xmzhiu.cn
+[cookie_path] => /
+[cookie_prefix] =>
+[default_m_layer] => Model
+[default_c_layer] => Action
+[default_app] => @
+[default_lang] => zh-cn
+[default_theme] => default
+[default_group] => appmanage
+[default_module] => Index
+[default_action] => index
+[default_charset] => utf-8
+[default_timezone] => PRC
+[default_ajax_return] => JSON
+[default_jsonp_handler] => jsonpReturn
+[default_filter] => htmlspecialchars
+[db_type] => mysql
+[db_host] => xmzhiuproduct.mysql.rds.aliyuncs.com
+[db_name] => jeesite
+[db_user] => api_touch
+[db_pwd] => fm9cAHCfKXiHjZxB4VgT
+[db_port] => 3306
+[db_prefix] =>
+[db_fieldtype_check] =>
+[db_fields_cache] => 1
+[db_charset] => utf8
+[db_deploy_type] => 0
+[db_rw_separate] =>
+[db_master_num] => 1
+[db_slave_no] =>
+[db_sql_build_cache] =>
+[db_sql_build_queue] => file
+[db_sql_build_length] => 20
+[db_sql_log] =>
+[data_cache_time] => 0
+[data_cache_compress] =>
+[data_cache_check] =>
+[data_cache_prefix] =>
+[data_cache_type] => File
+[data_cache_path] => /www/touch_xmzhiu_cn/tripmanage/Runtime/Temp/
+[data_cache_subdir] =>
+[data_path_level] => 1
+[error_message] => 您浏览的页面暂时发生了错误！请稍后再试～
+[error_page] =>
+[show_error_msg] =>
+[trace_exception] =>
+[log_record] => ture
+[log_type] => 3
+[log_dest] =>
+[log_extra] =>
+[log_level] => EMERG,ALERT,CRIT,ERR
+[log_file_size] => 2097152
+[log_exception_record] =>
+[session_auto_start] => 1
+[session_options] => Array
+(
+)
+
+[session_type] =>
+[session_prefix] =>
+[tmpl_content_type] => text/html
+[tmpl_action_error] => public:success
+[tmpl_action_success] => public:success
+[tmpl_exception_file] => ThinkPHP/Tpl/think_exception.tpl
+[tmpl_detect_theme] =>
+[tmpl_template_suffix] => .html
+[tmpl_file_depr] => /
+[url_case_insensitive] => 1
+[url_model] => 1
+[url_pathinfo_depr] => /
+[url_pathinfo_fetch] => ORIG_PATH_INFO,REDIRECT_PATH_INFO,REDIRECT_URL
+[url_html_suffix] =>
+[url_params_bind] => 1
+[url_404_redirect] =>
+[var_group] => g
+[var_module] => m
+[var_action] => a
+[var_ajax_submit] => ajax
+[var_jsonp_handler] => callback
+[var_pathinfo] => s
+[var_url_params] => _URL_
+[var_template] => t
+[var_filters] => filter_exp
+[output_encode] => 1
+[http_cache_control] => private
+[app_debug] =>
+[app_group_depr] => .
+[app_upload_path] => /upload/
+[img_upload_path] => /upload/imagecache/
+[fb_upload_path] => /upload/feedback/
+[supplier_bonus] => 50
+[tmpl_cache_on] =>
+[tmpl_engine_type] => Smarty
+[tmpl_l_delim] => <{
+[tmpl_r_delim] => }>
+[session_name] =>
+[session_expire] => 43200
+[session_path] => /www/touch_xmzhiu_cn/tripmanage/Runtime/Session/
+[user_auth_on] => 1
+[user_auth_type] => 1
+[user_auth_key] => TripUserKey
+[admin_auth_key] => TripAdminKey
+[require_auth_module] =>
+[not_auth_module] => Public
+[require_auth_action] =>
+[not_auth_action] =>
+[user_auth_gateway] => /Public/login
+[user_auth_model] => SysUser
+[rbac_role_table] => app_role
+[rbac_user_table] => app_role_user
+[rbac_access_table] => app_access
+[rbac_node_table] => app_node
+[rabc_menu_access] => app_menu_access
+[rabc_admin_menu] => app_admin_menu
+[page_rollpage] => 5
+[page_listrows] => 20
+[upload_cache_path] => /upload/imagecache
+[website_name] => 旅游电商一体机管理系统
+[website_url] => touch.xmzhiu.cn
+[user_default_header] => http://image.xmzhiu.cn/app_mobile/shop_header/default_buyer_header.jpg@!mobile-pc-header
+[oss_access_id] => lWnjmGOzMYVpTHj8
+[oss_access_key] => DbXEeJMy861QThZM8rc1Wn5gA5qQBW
+[ali_log] =>
+[ali_log_path] => /www/touch_xmzhiu_cn/ThinkPHP/Extend/Vendor/Aliyun/logs
+[ali_display_log] =>
+[ali_lang] => zh
+[ali_bucket] => zhiu
+[ali_upload_userheader] => app_mobile/user_header/
+[ali_upload_shopheader] => app_mobile/shop_header/
+[ali_upload_shopbanner] => app_mobile/shop_banner/
+[ali_upload_shopqrcode] => app_mobile/shop_qrcode/
+[ali_upload_productqrcode] => app_mobile/shop_product_qrcode/
+[ali_view_host] => http://image.xmzhiu.cn/
+[ali_upload_personal] => personal/
+[sms_ececcid] => 305703
+[sms_password] => Zhiujerry!
+[sms_type] => 5
+[sms_longcode] => zhiu
+[sms_sing] => 【知游】
+[sms_request_url] => http://pi.noc.cn/SendSMS.aspx
+[baidu_push_apikey] => P1CDbw8dnMhjszERpdWyOGim
+[baidu_push_secrekey] => UYQaMvVfGfmxFyUdXvz04VMMOu2F2N7j
+[baidu_push_ios_deploy] => 1
+[rongyun_appkey] => e0x9wycfxlvvq
+[rongyun_appsecret] => LqaA4izvOutx4
+[rongyun_header] => http://image.xmzhiu.cn/app_mobile/shop_header/default_buyer_header.jpg@!mobile-small
+[alipay_wap_partner] => 2088811922110672
+[alipay_wap_seller_id] => zcy@xmzhiu.com
+[alipay_wap_private_key] => /www/touch_xmzhiu_cn/ThinkPHP/Extend/Vendor/Alipaywap/rsa_private_key.pem
+[alipay_wap_public_key] => /www/touch_xmzhiu_cn/ThinkPHP/Extend/Vendor/Alipaywap/alipay_public_key.pem
+[alipay_wap_sign_type] => RSA
+[alipay_wap_charset] => utf-8
+[alipay_wap_cacert] => /www/touch_xmzhiu_cn/ThinkPHP/Extend/Vendor/Alipaywap/cacert.pem
+[alipay_wap_transport] => http
+[alipay_wap_notifyurl] => http://touch.xmzhiu.cn/ApiOrder/wapnotify
+[h5page_host] => http://m.xmzhiu.cn/
+[ali_refund_partner] => 2088811922110672
+[ali_refund_key] => ie5vhvkgpswuv7k05figz4fhk3ugah3t
+[ali_refund_sign_type] => MD5
+[ali_refund_input_charset] => utf-8
+[ali_refund_cacert] => /www/touch_xmzhiu_cn/ThinkPHP/Extend/Vendor/Alirefund/cacert.pem
+[ali_refund_transport] => http
+[ali_refund_notify_url] => http://touch.xmzhiu.cn/index.php/Public/refundNotify
+[ali_refund_account_name] => 厦门知游信息科技有限公司
+[ali_rrefund_seller_email] => zcy@xmzhiu.com
+[kuaidi100_key] => 113ab3137411f5a2
+[kuaidi100_url] => http://api.kuaidi100.com/api?
+[extends] => Array
+(
+[app_init] => Array
+(
+)
+
+[app_begin] => Array
+(
+[0] => ReadHtmlCache
+)
+
+[route_check] => Array
+(
+[0] => CheckRoute
+)
+
+[app_end] => Array
+(
+)
+
+[path_info] => Array
+(
+)
+
+[action_begin] => Array
+(
+)
+
+[action_end] => Array
+(
+)
+
+[view_begin] => Array
+(
+)
+
+[view_template] => Array
+(
+[0] => LocationTemplate
+)
+
+[view_parse] => Array
+(
+[0] => ParseTemplate
+)
+
+[view_filter] => Array
+(
+[0] => ContentReplace
+[1] => TokenBuild
+[2] => WriteHtmlCache
+[3] => ShowRuntime
+)
+
+[view_end] => Array
+(
+[0] => ShowPageTrace
+)
+
+)
+
+)
